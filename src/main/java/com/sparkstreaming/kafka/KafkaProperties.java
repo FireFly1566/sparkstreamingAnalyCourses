20b7c13da1f939f5280d3110e11ff6f3b61c7459
LoneSurvivor1993 <tanzebin@foxmail.com>
2019-03-08 19:43:39 +0800

    Kafka配置文件

--- src/main/java/com/sparkstreaming/kafka/KafkaProperties.java
+++ src/main/java/com/sparkstreaming/kafka/KafkaProperties.java
@@ -1,4 +1,20 @@
 package com.sparkstreaming.kafka;
 
+/**
+ * @param
+ * @Description: Kafka 常用配置文件
+ * @return
+ */
+
 public class KafkaProperties {
+
+    public static final String ZK = "node1:2181,node2:2181,node3:2181";
+
+    public static final String TOPIC = "my-replicated-topic";
+
+    public static final String BROKER_LIST = "node1:9093,node1:9094,node1:9095";
+
+
+
+
 }
