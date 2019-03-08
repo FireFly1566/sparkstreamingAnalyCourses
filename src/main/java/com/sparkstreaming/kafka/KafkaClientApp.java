b0d287f4acefec8378e36d0950c31184ff3dde9d
LoneSurvivor1993 <tanzebin@foxmail.com>
2019-03-08 22:28:56 +0800

    Kafka Java API 测试主类

--- src/main/java/com/sparkstreaming/kafka/KafkaClientApp.java
+++ src/main/java/com/sparkstreaming/kafka/KafkaClientApp.java
@@ -24,6 +24,9 @@ public class KafkaClientApp {
 
     public static void main(String[] args) {
         new KafkaProducer(KafkaProperties.TOPIC).start();
+
+        new KafkaConsumer(KafkaProperties.TOPIC).start();
+
     }
 
 }
